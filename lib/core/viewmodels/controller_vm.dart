7c67e4e29e6b356722fadfbe754168e5c44069bc
k1ycee <chiagozieani145@gmail.com>
2025-02-21 21:01:44 +0100

    Minor league tabs

--- lib/core/viewmodels/controller_vm.dart
+++ lib/core/viewmodels/controller_vm.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:iconsax/iconsax.dart';
 import 'package:mon_task/core/core.dart';
+import 'package:mon_task/views/home/tabs/fave_tab.dart';
+import 'package:mon_task/views/home/tabs/message_tab.dart';
+import 'package:mon_task/views/home/tabs/profile_tab.dart';
 
 
 class ControllerViewModel extends ChangeNotifier {
@@ -18,14 +21,14 @@ class ControllerViewModel extends ChangeNotifier {
     ControllerPageModel(
       icon: Iconsax.message5,
       name: 'Message',
-      page: Container(),
+      page: MessagesTab(),
     ),
     ControllerPageModel(icon: Iconsax.home5, name: 'Home', page: Container()),
-    ControllerPageModel(icon: Iconsax.heart5, name: 'Fave', page: Container()),
+    ControllerPageModel(icon: Iconsax.heart5, name: 'Fave', page: FaveTab()),
     ControllerPageModel(
       icon: Iconsax.profile_tick5,
       name: 'Profile',
-      page: Container(),
+      page: ProfileTab(),
     ),
   ];
 
