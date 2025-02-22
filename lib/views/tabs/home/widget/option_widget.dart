721af79e26d92eac09a2ad19824b7ad0c6df35cf
k1ycee <chiagozieani145@gmail.com>
2025-02-22 18:04:30 +0100

    Mode option animation smoother

--- lib/views/tabs/home/widget/option_widget.dart
+++ lib/views/tabs/home/widget/option_widget.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class OptionWidget extends StatefulWidget {
   const OptionWidget({
     super.key,
@@ -23,97 +22,111 @@ class OptionWidget extends StatefulWidget {
 }
 
 class _OptionWidgetState extends State<OptionWidget>
-    with SingleTickerProviderStateMixin {
-  late AnimationController _animationController;
-  late Animation<int> _animateCount;
+    with TickerProviderStateMixin {
+  late AnimationController _countAnimationController;
+  late Animation<int> _countAnimation;
 
-  bool _animateSize = false;
+  late AnimationController _containerAnimationController;
+  late Animation<double> _containerAnimation;
 
   @override
   void initState() {
     super.initState();
-    _animationController = AnimationController(
+
+    _containerAnimationController = AnimationController(
       vsync: this,
-      duration: const Duration(milliseconds: 1200),
+      duration: const Duration(milliseconds: 800),
     );
-    _animateCount = StepTween(
+    _containerAnimation = Tween<double>(
+      begin: 0,
+      end: 1,
+    ).animate(_containerAnimationController);
+    _countAnimationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 800),
+    );
+    _countAnimation = StepTween(
       begin: 0,
       end: widget.count,
-    ).animate(_animationController);
+    ).animate(_countAnimationController);
     WidgetsBinding.instance.addPostFrameCallback((timeStamp) async {
       await Future.delayed(const Duration(milliseconds: 3500));
       if (mounted) {
-        setState(() {
-          _animateSize = true;
-        });
-        _animationController.forward();
+        _containerAnimationController.forward();
+        _countAnimationController.forward();
       }
     });
   }
 
   @override
   void dispose() {
-    _animationController.dispose();
+    _countAnimationController.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return AnimatedScale(
-      scale: _animateSize ? 1 : 0,
-      duration: Duration(milliseconds: 300),
-      child: Container(
-        width: double.maxFinite,
-        padding: EdgeInsets.all(12),
-        decoration: BoxDecoration(
-          color: widget.color,
-          shape: widget.shape ?? BoxShape.rectangle,
-          borderRadius: widget.borderRadius
-        ),
-        child: Padding(
-         padding: const EdgeInsets.only(bottom: 10, left: 16, right: 16),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Text(
-                widget.option,
-                style: Theme.of(context).textTheme.bodyLarge!.copyWith(
-                  color: widget.textColor,
-                  fontSize: 16,
-                ),
-              ),
-              const SizedBox(height: 24),
-              Column(
+    return AnimatedBuilder(
+      animation: _containerAnimation,
+      builder: (context, _) {
+        return ScaleTransition(
+          scale: _containerAnimation,
+          child: Container(
+            width: double.maxFinite,
+            padding: EdgeInsets.all(12),
+            decoration: BoxDecoration(
+              color: widget.color,
+              shape: widget.shape ?? BoxShape.rectangle,
+              borderRadius: widget.borderRadius,
+            ),
+            child: Padding(
+              padding: const EdgeInsets.only(bottom: 10, left: 16, right: 16),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
                 children: [
-                  AnimatedBuilder(
-                    animation: _animationController,
-                    builder: (context, _) {
-                      return FittedBox(
-                        child: Text(
-                          '${_animateCount.value}',
-                          style: Theme.of(context).textTheme.bodyLarge!.copyWith(
-                            fontSize: 30,
-                            fontWeight: FontWeight.bold,
-                            color: widget.textColor,
-                          ),
-                        ),
-                      );
-                    },
-                  ),
                   Text(
-                    'Offers',
-                    style: Theme.of(context).textTheme.bodySmall!.copyWith(
-                      fontSize: 16,
+                    widget.option,
+                    style: Theme.of(context).textTheme.bodyLarge!.copyWith(
                       color: widget.textColor,
+                      fontSize: 16,
                     ),
                   ),
+                  const SizedBox(height: 24),
+                  Column(
+                    children: [
+                      AnimatedBuilder(
+                        animation: _countAnimationController,
+                        builder: (context, _) {
+                          return FittedBox(
+                            child: Text(
+                              '${_countAnimation.value}',
+                              style: Theme.of(
+                                context,
+                              ).textTheme.bodyLarge!.copyWith(
+                                fontSize: 30,
+                                fontWeight: FontWeight.bold,
+                                color: widget.textColor,
+                              ),
+                            ),
+                          );
+                        },
+                      ),
+                      Text(
+                        'Offers',
+                        style: Theme.of(context).textTheme.bodySmall!.copyWith(
+                          fontSize: 16,
+                          color: widget.textColor,
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 16),
                 ],
               ),
-              const SizedBox(height: 16),
-            ],
+            ),
           ),
-        ),
-      ),
+        );
+      },
     );
   }
 }
